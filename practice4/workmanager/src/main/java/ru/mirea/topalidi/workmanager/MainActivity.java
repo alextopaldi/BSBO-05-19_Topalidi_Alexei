0a4c8e277af8a81158781ba65445021a6768454c
alextopaldi <4lexey11@mail.ru>
2021-05-24 19:38:30 +0300

    Topalidi BSBO-05-19

---
+++ practice4/workmanager/src/main/java/ru/mirea/topalidi/workmanager/MainActivity.java
@@ -0,0 +1,27 @@
+package ru.mirea.topalidi.workmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WorkRequest uploadWorkRequest =
+                new OneTimeWorkRequest.Builder(UploadWorker.class)
+                        .build();
+        WorkManager
+                .getInstance(this)
+                .enqueue(uploadWorkRequest);
+    }
+}
